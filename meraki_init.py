54380578e4d10d50ce25257ef5431c78409b2c36
Gabriel Zapodeanu <gzapodea@cisco.com>
2017-04-19 22:13:16 -0700

    Create meraki_init.py

---
+++ meraki_init.py
@@ -0,0 +1,9 @@
+
+
+# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
+
+# !/usr/bin/env python3
+
+
+MERAKI_API_KEY = 'Put your Meraki API key here'
+MERAKI_ORG_ID = 'Put your Meraki Organization ID here'
