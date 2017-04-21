3b49b67d2b03c490e80d2fd616d37d25e2d09e1d
gzapodea <gzapodea@cisco.com>
2017-04-20 17:18:14 -0700

    work in progress

--- meraki_SM_location.py
+++ meraki_SM_location.py
@@ -17,6 +17,11 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable in
 
 # The following declarations need to be updated based on your lab environment
 
+users_info_list = [{'name': 'Gabi Zapodeanu', 'email': 'gzapodea@cisco.com', 'cell': '+15033094949'},
+                   {'name': 'Gabriel Zapodeanu', 'email': 'gabriel.zapodeanu@gmail.com', 'cell': '+15036252333'}]
+
+user_email = 'gzapodea@cisco.com'
+
 MERAKI_URL = 'https://dashboard.meraki.com/api/v0'
 
 
@@ -73,12 +78,42 @@ def meraki_get_sm_devices(network_id):
     print(url)
     header = {'content-type': 'application/json', 'X-Cisco-Meraki-API-Key': MERAKI_API_KEY}
     sm_devices_response = requests.get(url, headers=header, verify=False)
-    sm_devices_json = sm_devices_response.json()
-    pprint(sm_devices_json)
+    sm_devices_json = sm_devices_response.json()['devices']
+    return sm_devices_json
+
+
+def get_user_cell(users_info, email):
+    """
+    This function will look up the user cell phone based on his email
+    :param users_info: List of all the users info
+    :param user_email: user email address
+    :return: the user cell phone number
+    """
+
+    user_cell = None
+    for user in users_info:
+        if user['email'] == email:
+            user_cell = user['cell']
+    return user_cell
+
+
+def get_location_cell(sm_devices_list, user_cell):
+    """
+    This function will locate the user based on his cell phone number
+    :param sm_devices_list: the list of Meraki SM devices
+    :param user_cell: user cell phone number
+    :return: the user location
+    """
+    location = None
+    for device in sm_devices_list:
+        if device['phoneNumber'] == user_cell:
+            location = device['location']
+    return location
 
 
 def main():
 
+    pprint(users_info_list)
     # get the Meraki organization id
 
     meraki_org_id = meraki_get_organizations()
@@ -86,16 +121,28 @@ def main():
 
     # get the Meraki networks info
 
-    network_info = meraki_get_networks(meraki_org_id)
-    meraki_network_id = network_info[0]
-    meraki_network_name = network_info[1]
+    meraki_network_info = meraki_get_networks(meraki_org_id)
+    meraki_network_id = meraki_network_info[0]
+    meraki_network_name = meraki_network_info[1]
 
     print('Your Meraki Network ID is: ', meraki_network_id)
     print('Your Meraki Network Name is: ', meraki_network_name)
 
     # get the Meraki SM devices
 
-    meraki_get_sm_devices(meraki_network_id)
+    meraki_sm_devices_list = meraki_get_sm_devices(meraki_network_id)
+    print('Your Meraki SM Devices list: \n')
+    pprint(meraki_sm_devices_list)
+
+    # find out the user cell phone number based on email address
+
+    user_cell = get_user_cell(users_info_list, user_email)
+    print('The SM user with the email ', user_email, ' has the cell phone number ', user_cell)
+
+    # find the location for the device with the specified cell phone number
+
+    user_location = get_location_cell(meraki_sm_devices_list,user_cell)
+    print('The SM user with the cell phone ', user_cell, ' is located at this address ', user_location)
 
 
 if __name__ == '__main__':
